dbb9a4e2c6462ef78e76be070e7eb5d73eb4938b
soCalled devloper <111551752+Yuke2003@users.noreply.github.com>
2024-07-11 17:50:26 +0530

    Update emailController.js

--- app/utils/emailController.js
+++ app/utils/emailController.js
@@ -1,5 +1,6 @@
-// utils/sendEmail.js
+
 "use server";
+
 import sgMail from "@sendgrid/mail";
 import dotenv from "dotenv";
 
@@ -7,25 +8,33 @@ dotenv.config({ path: "./.env.local" });
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-exports.sendEmail = async (req, res, next) => {
+export const sendEmail = async ({
+  fromEmail,
+  toEmail,
+  sendSubject,
+  sendText,
+}) => {
   try {
-    const { fromEmail, toEmail, sendSubject, sendText, sendHtml } = req.body;
-   
+    const sendHtml = `<p>${sendText}</p>`;
     const msg = {
       to: toEmail,
-      from: fromEmail, 
+      from: fromEmail,
       subject: sendSubject,
       text: sendText,
       html: sendHtml,
     };
     await sgMail.send(msg);
+    console.log(msg);
     console.log("Email sent successfully!");
+    return { status: "success", message: "Email sent successfully!" };
   } catch (error) {
     console.error("Error sending email:", error);
-
     if (error.response) {
       console.error("SendGrid response:", error.response.body);
+      return { status: "error", message: error.response.body };
+    } else {
+      return { status: "error", message: "Error sending email" };
     }
   }
-  next();
 };
+
