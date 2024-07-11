85189324810766ab0dd6f2dea8accf6f66efaf00
soCalled devloper <yukeshmeganathan2003@gmail.com>
2024-07-11 18:58:56 +0530

    first commit

--- app/components/EmailHandler.js
+++ app/components/EmailHandler.js
@@ -1,63 +1,76 @@
 "use client";
 
-import { useRef, useState } from "react";
-import emailjs from "@emailjs/browser";
+import { useState } from "react";
+import { sendEmail } from "@/app/utils/emailController"; // Update the path accordingly
+
 function EmailHandler() {
-  const form = useRef();
-  const [sentMessage, setSentMessage] = useState();
-  const sendEmail = (e) => {
+  const [formData, setFormData] = useState({
+    fromEmail: "yukeshmeganathan2003@gmail.com",
+    toEmail: "",
+    sendSubject: "",
+    sendText: "",
+  });
+  const [sentMessage, setSentMessage] = useState("");
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    emailjs
-      .sendForm("service_uzrwk21", "template_c91rxsn", form.current, {
-        publicKey: "oosRe9Lw094wJzp3C",
-      })
-      .then(
-        () => {
-          console.log("SUCCESS!");
-          console.log("message sent");
-          setSentMessage("email Sent successfully");
-        },
-        (error) => {
-          console.log("FAILED...", error.text);
-        }
-      );
+
+    const result = await sendEmail(formData);
+    if (result) {
+      setSentMessage(result.message);
+    } else {
+      setSentMessage(result.message);
+    }
   };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="flex flex-col items-center justify-center p-6 mx-8">
       <h1 className="mt-20 text-4xl font-medium bg-[#f8f062] p-4 rounded-2xl">
         Send an email for web services
       </h1>
-      <form
-        className="flex gap-20 bg-white shadow-xl p-12 mt-6 rounded-xl"
-        ref={form}
-        onSubmit={sendEmail}
-      >
+      <form className="flex gap-20 bg-white shadow-xl p-12 mt-6 rounded-xl">
         <div className="flex flex-col items-center gap-5">
           <input
             type="text"
-            placeholder="Enter an name"
-            name="user_name"
+            placeholder="Enter an Email"
+            name="toEmail"
+            value={formData.toEmail}
+            onChange={handleChange}
             className="w-[670px] bg-gray-200 border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-200"
           />
           <input
-            type="email"
-            placeholder="Enter an Email"
-            name="user_email"
+            type="text"
+            placeholder="Enter a Subject"
+            name="sendSubject"
+            value={formData.sendSubject}
+            onChange={handleChange}
             className="w-[670px] bg-gray-200 border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-200"
           />
           <textarea
             placeholder="Enter your web services requirement..."
-            name="message"
+            name="sendText"
+            value={formData.sendText}
+            onChange={handleChange}
             className="w-[670px] bg-gray-200 h-32 border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-200"
           />
         </div>
 
         <div>
-          <input
+          <button
+            className="bg-[#444] p-3 text-white font-thin w-80 rounded-lg"
             type="submit"
-            value="@Send an Email"
-            className="bg-[#444] p-3 text-white font-thin w-80 rounded-lg cursor-pointer"
-          />
+            onClick={handleSubmit}
+          >
+            @Send an Email
+          </button>
           <h3 className="mt-3 text-center">{sentMessage}</h3>
         </div>
       </form>
@@ -66,3 +79,4 @@ function EmailHandler() {
 }
 
 export default EmailHandler;
+
